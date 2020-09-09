438e0f365a3f48b817ba1ad799e45d3104646810
mpryor <mapryor1991@yahoo.com>
2020-09-09 13:41:55 -0500

    updated set-me-up.sh

--- set-me-up.sh
+++ set-me-up.sh
@@ -1,8 +1,21 @@
-mv .vim ~/
-mv ~/.zshrc ~/
+remove_if_exists() {
+	if [ -d $1 ]; then 
+		rm -rf $1;
+	fi
 
-rm ~/.config/terminator/config
+	if [ -f $1 ]; then 
+		rm $1;
+	fi
+}
+
+remove_if_exists ~/.vim
+mv .vim ~/
+
+remove_if_exists ~/.zshrc
+cp .zshrc ~/
+
+remove_if_exists ~/.config/terminator/config
 cp terminator-config ~/.config/terminator/config
 
-rm ~/.config/Code/User/settings.json
+remove_if_exists ~/.config/Code/User/settings.json
 cp vscode-settings.json ~/.config/Code/User/settings.json
