8f514d9d7e86805b6223a29ecc587bc328fe5f42
{'cmd': ['whoami'], 'stdout': 'matt', 'stderr': '', 'rc': 0, 'start': '2020-09-09 14:15:40.759698', 'end': '2020-09-09 14:15:40.761165', 'delta': '0:00:00.001467', 'changed': True, 'stdout_lines': ['matt'], 'stderr_lines': [], 'failed': False} <mapryor1991@yahoo.com>
2020-09-09 14:28:22 -0500

    fixed set-me-up script

--- set-me-up.sh
+++ set-me-up.sh
@@ -15,7 +15,13 @@ remove_if_exists ~/.zshrc
 cp .zshrc ~/
 
 remove_if_exists ~/.config/terminator/config
+if [ ! -d ~/.config/terminator ]; then
+	mkdir -p ~/.config/terminator
+fi
 cp terminator-config ~/.config/terminator/config
 
 remove_if_exists ~/.config/Code/User/settings.json
+if [ ! -d ~/.config/Code/User ]; then
+	mkdir -p ~/.config/Code/User
+fi
 cp vscode-settings.json ~/.config/Code/User/settings.json
