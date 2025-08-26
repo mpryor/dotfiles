6b3b30936c64862ab4ae2bcca5bbe8da1b1a979f
Matt Pryor <mapryor1991@yahoo.com>
2025-08-26 08:19:42 -0600

    chore: formatting

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -5,23 +5,23 @@ vim.g.maplocalleader = " "
 -- bootstrap our plugin manager, lazy --
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	})
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	import='plugins'
+    import = 'plugins'
 }, {
-	change_detection = {
-		notify = false,
-	},
+    change_detection = {
+        notify = false,
+    },
 })
 
 -- import local lua files --
