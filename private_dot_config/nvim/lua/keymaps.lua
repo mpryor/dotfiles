239a4085d06dea5870b2bfd96943c3a4d49680e4
mpryor <mapryor1991@yahoo.com>
2025-10-02 15:09:36 -0600

    feat: make F/f work like regular again

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -210,6 +210,11 @@ set("n", "n", "nzzzv:set hlsearch<CR>", { desc = "Keep (next) search result cent
 -- Repeat movement with ; and ,
 vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
 vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+-- Set up default f, F, t, T to be repeatable
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 
 vim.keymap.set('', '[-', '<Plug>(IndentWisePreviousLesserIndent)', {})
 vim.keymap.set('', '[=', '<Plug>(IndentWisePreviousEqualIndent)', {})
